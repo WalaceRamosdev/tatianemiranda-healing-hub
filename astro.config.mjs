2bb946be6b91a3f0e6b05fa5c644a230029c0401
Walace Ramos <contatowalace.dev@gmail.com>
2026-02-23 20:11:08 -0300

    refatorando página por completo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://tatiane-miranda-psicologa.vercel.app',
-  integrations: [tailwind(), sitemap()],
+    output: 'static',
 });
